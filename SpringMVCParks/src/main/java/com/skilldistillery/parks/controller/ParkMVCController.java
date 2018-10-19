4a998431d91e0ab16b4db10f9135017df85bb187
papadini <krpaladini@gmail.com>
2018-10-19 14:35:19 -0600

    stub controller

---
+++ SpringMVCParks/src/main/java/com/skilldistillery/parks/controller/ParkMVCController.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.parks.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ParkMVCController {
+	
+	
+	
+
+}
--- SpringMVCParks/src/main/java/com/skilldistillery/parks/data/ParkDAOImpl.java
+++ SpringMVCParks/src/main/java/com/skilldistillery/parks/data/ParkDAOImpl.java
@@ -1,7 +1,14 @@
 package com.skilldistillery.parks.data;
 
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Component;
+
 import com.skilldistillery.parks.entities.Park;
 
+
+@Transactional
+@Component
 public class ParkDAOImpl implements ParkDAO {
 
 	
